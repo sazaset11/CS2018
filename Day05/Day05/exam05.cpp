eb0dede71eed0a039ba650fb2c00a9a60e923632
sazaset11 <sazaset12@gmail.com>
2018-07-04 16:55:34 +0900

    Day05 modified

--- Day05/Day05/exam05.cpp
+++ Day05/Day05/exam05.cpp
@@ -1,6 +1,4 @@
 ﻿#include "stdafx.h"
-#include <string>
-#include <stdlib.h>
 
 int cTilteMap1[64] = {
 	1, 1, 1, 1, 1, 1, 1, 1,
@@ -12,7 +10,7 @@ int cTilteMap1[64] = {
 	1, 0, 0, 0, 0, 0, 0, 1,
 	1, 1, 1, 1, 1, 1, 1, 1
 };
-int playerPosX, playerPosY;
+int playerPosX, playerPosY, OldX, OldY;
 
 void printMap() {
 	cTilteMap1[playerPosX + playerPosY * 8] = 9;
@@ -47,19 +45,144 @@ void printMap() {
 	}
 }
 
+void getEvent(char *com) {
+	char m = com[0];
+	char check;
+	static int count = 0;
+	static bool tell_switch = true;
+	OldX = playerPosX;
+	OldY = playerPosY;
+
+	switch (m)
+	{
+	case 'w':
+		playerPosY--;
+		break;
+	case 's':
+		playerPosY++;
+		break;
+	case 'a':
+		playerPosX--;
+		break;
+	case 'd':
+		playerPosX++;
+		break;
+	default:
+		break;
+	}
+
+	switch (cTilteMap1[playerPosX + playerPosY * 8])
+	{
+	case 0:
+		switch (m)
+		{
+		case 'w':
+			printf("플레이어가 위쪽으로 이동합니다.\n");
+			break;
+		case 's':
+			printf("플레이어가 아래쪽으로 이동합니다.\n");
+			break;
+		case 'a':
+			printf("플레이어가 왼쪽으로 이동합니다.\n");
+			break;
+		case 'd':
+			printf("플레이어가 오른쪽으로 이동합니다.\n");
+			break;
+		default:
+			break;
+		}
+		cTilteMap1[OldX + OldY * 8] = 0;
+		break;
+	case 1:
+		printf("막힌길 입니다.\n");
+		playerPosX = OldX;
+		playerPosY = OldY;
+		break;
+	case 2:
+		cTilteMap1[OldX + OldY * 8] = 0;
+		system("cls");
+		printMap();
+		printf("탈출 성공~~!!! 수고하셨습니다.\n");
+
+		return;
+	case 3:
+		if (tell_switch) {
+			printf("전화가 울리고있습니다. 받으시겠습니까?(y/n)");
+			scanf_s("%c", &check, sizeof(check));
+			getchar();
+
+			if (check == 'y') {
+				printf("전화는 바로 끊어지고 무언가 떨어지는 소리가 났다.\n");
+				switch (m)
+				{
+				case 'w':
+					cTilteMap1[10] = 4;
+					break;
+				case 's':
+					cTilteMap1[26] = 4;
+					break;
+				case 'a':
+					cTilteMap1[17] = 4;
+					break;
+				case 'd':
+					cTilteMap1[19] = 4;
+					break;
+				default:
+					break;
+				}
+				tell_switch = false;
+			}
+			else {
+				count++;
+
+				if (count > 3) {
+					printf("전화가 끊어졌다\n그 이후로는 아무일도 일어나지 않아 시간만이 지나갔다.\n");
+					printf("GAME OVER\n");
+					return;
+				}
+
+			}
+		}
+		else {
+			printf("전화기가 놓여있다.\n");
+		}
+
+		playerPosX = OldX;
+		playerPosY = OldY;
+		break;
+	case 4:
+		printf("스위치가 놓여있습니다. 누르시겠습니까? (y,n)");
+		scanf_s("%c", &check, sizeof(check));
+		getchar();
+
+		if (check == 'y') {
+			printf("무거운 무언가가 움직이는 소리가 들렸다.\n");
+			cTilteMap1[55] = 2;
+			cTilteMap1[OldX + OldY * 8] = 0;
+		}
+		else {
+			playerPosX = OldX;
+			playerPosY = OldY;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
 int main() {
 	char szCmd[32];
-	bool tell_switch = true;
-	int count = 0;
 	//bool switch_switch = true;
 	playerPosX = 2;
 	playerPosY = 5;
+	
 
-	while (1) {
+	while ((playerPosX + playerPosY * 8 != 55) && (playerPosX + playerPosY * 8 != 18)) {
 		system("cls");
+
 		cTilteMap1[playerPosX + playerPosY * 8] = 9;
 		printMap();
-		printf("커멘드를 입력하세요 : ");
+		printf("커멘드를 입력하세요(help) : ");
 		scanf_s("%s", szCmd, sizeof(szCmd));
 		getchar();
 
@@ -67,11 +190,26 @@ int main() {
 			printf("프로그램을 종료 합니다.\n");
 			break;
 		}
-		else if (!strcmp(szCmd, "move")) {
+		else if (!strcmp(szCmd, "w")) {
+			getEvent(szCmd);
+		}
+		else if (!strcmp(szCmd, "a")) {
+			getEvent(szCmd);
+		}
+		else if (!strcmp(szCmd, "s")) {
+			getEvent(szCmd);
+		}
+		else if (!strcmp(szCmd, "d")) {
+			getEvent(szCmd);
+		}
+		/*else if (!strcmp(szCmd, "move")) {
 			while (true) {
 				system("cls");
+
 				cTilteMap1[playerPosX + playerPosY * 8] = 9;
+
 				printMap();
+
 				char m, check;
 				int x, y;
 				x = playerPosX;
@@ -199,6 +337,14 @@ int main() {
 				}
 				system("pause");
 			}
+		}*/
+		else if (!strcmp(szCmd, "help")) {
+			printf("--커맨드 목록--\n");
+			printf("quit : 종료\n");
+			printf("w : 위로 이동\n");
+			printf("a : 왼쪽으로 이동\n");
+			printf("s : 아래로 이동\n");
+			printf("d : 오른쪽으로 이동\n");
 		}
 			
 		/*else if (!strcmp(szCmd, "dump")) {
